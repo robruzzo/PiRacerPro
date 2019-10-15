ce1663288a855762e9d1692ca3c21b25aa850ce2
waveshare <36142105+waveshare@users.noreply.github.com>
2019-10-15 23:27:26 +0800

    Update cfg_complete.py

--- donkeycar/templates/cfg_complete.py
+++ donkeycar/templates/cfg_complete.py
@@ -34,8 +34,8 @@ CAMERA_FRAMERATE = DRIVE_LOOP_HZ
 CSIC_CAM_GSTREAMER_FLIP_PARM = 0 # (0 => none , 4 => Flip horizontally, 6 => Flip vertically)
 
 #9865, over rides only if needed, ie. TX2..
-PCA9685_I2C_ADDR1 = 0x40     #steering I2C address, use i2cdetect to validate this number
-PCA9685_I2C_ADDR2 = 0x40     #throttle I2C address, use i2cdetect to validate this number
+PCA9685_I2C_ADDR = 0x40     #steering I2C address, use i2cdetect to validate this number
+PCA9685_I2C_ADDR1 = 0x40    #throttle I2C address, use i2cdetect to validate this number
 PCA9685_I2C_BUSNUM = None   #None will auto detect, which is fine on the pi. But other platforms should specify the bus num.
 
 #DRIVETRAIN
